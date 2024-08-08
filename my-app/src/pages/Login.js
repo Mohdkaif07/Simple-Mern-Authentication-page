5506a431c4d5f1d91ef71ad26ac6dbc484b53c80
Mohd kaif <155153843+Mohdkaif07@users.noreply.github.com>
2024-08-09 02:31:56 +0530

    Update Login.js

--- my-app/src/pages/Login.js
+++ my-app/src/pages/Login.js
@@ -9,7 +9,7 @@ function App() {
 
   async function loginUser(event){
   event.preventDefault()
-  const response = await fetch('http://localhost:1337/api/login', {
+  const response = await fetch('https://simple-mern-authentication-page-2.onrender.com/', {
   headers: {
     'Content-Type':'application/JSON',
   },
