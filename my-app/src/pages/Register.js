28104d571fcd082d97bdf426e7c6c7b00d5b8d29
Mohd kaif <155153843+Mohdkaif07@users.noreply.github.com>
2024-08-09 02:49:20 +0530

    Update Register.js

--- my-app/src/pages/Register.js
+++ my-app/src/pages/Register.js
@@ -14,7 +14,7 @@ function App() {
 
    async function registerUser(event){
          event.preventDefault()
-        const response = await fetch('https://simple-mern-authentication-page-2.onrender.com/', {
+        const response = await fetch('https://simple-mern-authentication-page-2.onrender.com/api/register', {
        headers: {
         'Content-Type':'application/JSON',
          },
